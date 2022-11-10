874e1da11ef1f7585557a5f851639b776715bd68
TaniaCruz09 <cruztania0909@gmail.com>
2022-11-10 14:59:57 -0600

    first commit

--- router.ts
+++ router.ts
@@ -1,16 +1,12 @@
 import { Express } from "express";
 import authRouter from "./auth/router";
 import productsRouter from "./products/router";
-import categoriesRouter from "./categories/router";
-import tagsRouter from "./tags/router";
 import saleRouter from "./sale/router";
 import suplierRouter from "./suplier/router";
 
 const router = (app: Express) => {
   app.use("/auth", authRouter);
   app.use("/products", productsRouter);
-  app.use("/categories", categoriesRouter);
-  app.use("/tags", tagsRouter);
   app.use("/sale", saleRouter);
   app.use("/suplier", suplierRouter);
 };
